fd12724ca6531ac33e95e8d008ad8a17f892a112
IshaqNiloy <ishaqniloy1996@gmail.com>
2018-02-26 15:50:53 +0600

    Add files via upload

---
+++ jquery/jquery_4.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+	$(".btn1,.btn2").click(function(){
+		$(".slide").slideToggle(1100);
+	});
+});
+$(document).ready(function(){
+	$(".btn1").click(function(){
+		$(".btn1").hide(2000);
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
